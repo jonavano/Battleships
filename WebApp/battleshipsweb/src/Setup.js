c25f5179da9fd72314735de485e865eff568829e
Ethoes <calvinbre1999@hotmail.com>
2020-09-10 20:58:53 +0200

    Added easy board management
    Made boards easily re sizable and modular.

--- WebApp/battleshipsweb/src/Setup.js
+++ WebApp/battleshipsweb/src/Setup.js
@@ -1,26 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Setup.css';
 
-function Setup() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Setup extends React.Component {
+  render() {
+    return (
+        <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+ }
+}
+
+class Board  extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    renderRow(j, k) {
+        var result = [];
+        var i;
+        for(i = 0; i < j; i++) {
+            result.push(this.renderSquare(i + k));
+        }
+        return result;
+    }
+
+    renderBoard(j) {
+        var i;
+        var result = [];
+        for(i = 0; i < j; i++) {
+            result.push(<div classname="board-row"> {this.renderRow(j,i * j)} </div>);
+        }
+        return result;
+    }
+
+    render() {
+        return(
+            <div>
+                {this.renderBoard(7)}
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component {
+    render() {
+        return(
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}>
+                {this.props.value}      
+            </button>
+        );
+    }
 }
 
 export default Setup;
